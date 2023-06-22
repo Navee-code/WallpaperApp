3ab1afa9902698306c27183546c274fb755f7028
Naveen <balaji@brochill.com>
2023-06-22 18:36:57 +0530

    setUp commit

---
+++ app/src/main/java/com/example/wallpaper/HomePageAdapterCategory.kt
@@ -0,0 +1,24 @@
+package com.example.wallpaper
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HomePageAdapterCategory ():RecyclerView.Adapter<HomePageAdapterCategory.ImageViewHolder>(){
+    class ImageViewHolder(view:View):RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
