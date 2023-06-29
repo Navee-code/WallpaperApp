9544719fb274911047729b6182bf226f4583343d
Naveen <balaji@brochill.com>
2023-06-29 19:07:58 +0530

    fragament init added

--- app/src/main/java/com/example/wallpaper/HomePageFragment.kt
+++ app/src/main/java/com/example/wallpaper/HomePageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,7 +35,8 @@ class HomePageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+//
         return inflater.inflate(R.layout.fragment_home_page, container, false)
     }
 
--- app/src/main/java/com/example/wallpaper/MainActivity.kt
+++ app/src/main/java/com/example/wallpaper/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.wallpaper
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.NavController
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+             findViewById<Button>(R.id.firebase).setOnClickListener {
+                 val database = Firebase.database
+                 val myRef = database.getReference("message")
+                 myRef.setValue("Hello, World!")
+             }
+
     }
 }
